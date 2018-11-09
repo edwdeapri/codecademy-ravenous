53b3500713283a80623d055bb0c9e13af213ff00
Edward Pritchard <edward.d.pritchard@gmail.com>
2018-11-09 16:08:46 -0700

    docs: add/update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-
